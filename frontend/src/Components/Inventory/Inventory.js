328c6b10d2f8e6190d53b5c862a609fe5cf2cb52
Spencer <spencerm133@gmail.com>
2018-09-15 21:42:55 -0400

    hola

--- frontend/src/Components/Inventory/Inventory.js
+++ frontend/src/Components/Inventory/Inventory.js
@@ -18,17 +18,19 @@ class Inventory extends Component {
     fetchSuppliers = (value) => {
       fetch('http://api.medwithoutborders.org/medicine', {
       method: 'post',
-      body: JSON.stringify({
-        id: value,
-        uuid: localStorage.getItem('uuid'),
-      })
+      body: JSON.stringify(
+        {
+          id: value,
+          uuid: localStorage.getItem('uuid'),
+        }
+      )
     })
         .then(res => res.json())
         .then(data => this.setState({ inventory : data, isLoading : false, }))
         
      // this.setState({ inventory: [{"id":"5b9cdc54cb18d0095c485dce","name":"Best Buy","quantity":30,"price_per_unit":10.3, medType: 'C0001'},{"id":"5b9cdc73cb18d00ddb4a2552","name":"Best Buy","quantity":30,"price_per_unit":10.3},{"id":"5b9cddd7cb18d01da2a9e2fa","name":"Best Buy","quantity":30,"price_per_unit":10.3},{"id":"5b9cde42cb18d022dd02f356","name":"Best Buy","quantity":30,"price_per_unit":10.3}]  })
     }
-  }
+  
 
     componentDidMount () {
       //this.fetchSuppliers();
