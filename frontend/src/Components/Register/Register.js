f2de9d21542e83f4d5f92c9c874e768d2e149ccf
MorganGallant <morgan@morgangallant.com>
2018-09-16 00:23:31 -0400

    Fixed type

--- frontend/src/Components/Register/Register.js
+++ frontend/src/Components/Register/Register.js
@@ -66,7 +66,7 @@ class Register extends React.Component {
 			.then(response => response.json())
 			.then(user => {
 				localStorage.setItem('uuid', user[1]);
-				localStorage.setItem('token', data[2]);
+				localStorage.setItem('token', user[2]);
 				this.props.history.push("/");
 			})
 	}
